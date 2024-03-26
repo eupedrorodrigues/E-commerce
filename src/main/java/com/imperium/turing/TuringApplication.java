3fdaf087b35eddba6e1ab9a6f838228fbe4db978
Pedro Ricardo <pedrorsrodrigues1203@gmail.com>
2024-03-26 17:46:20 -0300

    Initial commit

---
+++ src/main/java/com/imperium/turing/TuringApplication.java
@@ -0,0 +1,13 @@
+ package com.imperium.turing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imperium/turing/TuringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imperium.turing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
