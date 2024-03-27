0948760de5dc23bd11104f16f1d3c639feb7eecd
Pedro Ricardo <pedrorsrodrigues1203@gmail.com>
2024-03-27 14:48:42 -0300

    Feat: H2 database, test profile, JPA

--- src/main/java/com/imperium/turing/entities/User.java
+++ src/main/java/com/imperium/turing/entities/User.java
@@ -3,18 +3,26 @@ package com.imperium.turing.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
-	
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable {	
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	public User() {
-		
-	}
+	
 	
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
