dd4be85955be7bbef8750a8808e2669e2c4f6ddc
Pedro Ricardo <pedrorsrodrigues1203@gmail.com>
2024-03-27 15:26:02 -0300

    Feat: JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/imperium/turing/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.imperium.turing.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.imperium.turing.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	
+	
+}
