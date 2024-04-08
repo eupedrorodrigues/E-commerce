3b2da9496ef98ca6c388aee5df5a38a3d64fe915
Pedro Ricardo <pedrorsrodrigues1203@gmail.com>
2024-04-08 18:07:01 -0300

    Feat: Exception handling - delete

--- src/main/java/com/imperium/turing/services/UserService.java
+++ src/main/java/com/imperium/turing/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.imperium.turing.entities.User;
 import com.imperium.turing.repositories.UserRepository;
+import com.imperium.turing.services.exceptions.DatabaseException;
 import com.imperium.turing.services.exceptions.ResourceNotFoundException;
 
 
@@ -31,7 +34,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User udpate(Long id, User obj) {
